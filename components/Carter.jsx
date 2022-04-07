77f6ea5831de249873fd053c32d7d4ee3fd6729c
rajaaltus <rajaaltus@gmail.com>
2022-04-07 20:56:34 +0700

    componetized

--- components/Carter.jsx
+++ components/Carter.jsx
@@ -1,40 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useCart } from "../hooks/useCart";
+import { TrashIcon } from "@heroicons/react/solid";
 
-const Carter = ({ addItemToCart, removeItemFromCart, id }) => {
-  const [isActive, setIsActive] = useState(false);
-  const { cartItems } = useCart();
-  const handleIncrement = () => {
-    setIsActive(true);
-    addItemToCart();
+const Carter = ({ product }) => {
+  const [isInCart, setIsInCart] = useState(false);
+  const { increase, addProduct, decrease, cartItems, removeProduct } =
+    useCart();
+
+  const getQty = (product) => {
+    return (
+      cartItems[cartItems.findIndex((item) => item.id === product.id)] &&
+      cartItems[cartItems.findIndex((item) => item.id === product.id)].qty
+    );
   };
-  const handleDecrement = () => {
-    removeItemFromCart();
-  };
+
+  useEffect(() => {
+    const check =
+      cartItems[cartItems.findIndex((item) => item.id === product.id)] || null;
+    check ? setIsInCart(true) : setIsInCart(false);
+  }, [cartItems, product.id]);
 
   return (
-    <div className="absolute top-3 right-4">
-      <div className="flex items-center justify-between  flex-row-reverse">
-        <button
-          className="w-10 h-10 bg-green-600 text-white rounded-full text-2xl font-semibold "
-          onClick={handleIncrement}
-        >
-          +
-        </button>
-        {isActive && (
-          <>
-            {cartItems && cartItems[id] && (
-              <span className="px-4">
-                {cartItems && cartItems[id] && cartItems[id].qty}
-              </span>
-            )}
-            <button
-              className="w-10 h-10 bg-green-600 text-white rounded-full text-2xl font-semibold"
-              onClick={handleDecrement}
-            >
-              -
-            </button>
-          </>
+    <div className="absolute top-3 right-3">
+      <div className="flex items-center justify-between flex-row-reverse">
+        {isInCart && getQty(product) > 0 && (
+          <button className="btn" onClick={() => increase(product)}>
+            +
+          </button>
+        )}
+
+        {!isInCart && (
+          <button className="btn-lg" onClick={() => addProduct(product)}>
+            Add
+          </button>
+        )}
+
+        {isInCart && <span className="px-4">{getQty(product)}</span>}
+
+        {isInCart && getQty(product) > 1 && (
+          <button className="btn" onClick={() => decrease(product)}>
+            -
+          </button>
+        )}
+
+        {isInCart && getQty(product) === 1 && (
+          <button
+            className="trash btn flex items-center justify-center"
+            onClick={() => removeProduct(product)}
+          >
+            <TrashIcon className="w-6 h-6" />
+          </button>
         )}
       </div>
     </div>
--- components/Product.jsx
+++ components/Product.jsx
@@ -1,25 +1,9 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { useCart } from "../hooks/useCart";
-import { TrashIcon } from "@heroicons/react/solid";
+import Carter from "./Carter";
 
 const Product = ({ product }) => {
-  const [isInCart, setIsInCart] = useState(false);
   const { name, price } = product;
-  const { increase, addProduct, decrease, cartItems, removeProduct } =
-    useCart();
-  const getQty = (product) => {
-    return (
-      cartItems[cartItems.findIndex((item) => item.id === product.id)] &&
-      cartItems[cartItems.findIndex((item) => item.id === product.id)].qty
-    );
-  };
-
-  useEffect(() => {
-    const check =
-      cartItems[cartItems.findIndex((item) => item.id === product.id)] || null;
-    check ? setIsInCart(true) : setIsInCart(false);
-  }, [cartItems, product.id]);
 
   return (
     <div className="relative w-full h-96 bg-white rounded-lg shadow-2xl">
@@ -36,35 +20,7 @@ const Product = ({ product }) => {
           <p className="text-lg font-bold text-gray-500">$ {price}</p>
         </div>
       </div>
-
-      <div className="absolute top-3 right-3">
-        <div className="flex items-center justify-between flex-row-reverse">
-          {isInCart && getQty(product) > 0 && (
-            <button className="btn" onClick={() => increase(product)}>
-              +
-            </button>
-          )}
-          {!isInCart && (
-            <button className="btn-lg" onClick={() => addProduct(product)}>
-              Add
-            </button>
-          )}
-          {isInCart && <span className="px-4">{getQty(product)}</span>}
-          {isInCart && getQty(product) > 1 && (
-            <button className="btn" onClick={() => decrease(product)}>
-              -
-            </button>
-          )}
-          {isInCart && getQty(product) === 1 && (
-            <button
-              className="trash btn flex items-center justify-center"
-              onClick={() => removeProduct(product)}
-            >
-              <TrashIcon className="w-6 h-6" />
-            </button>
-          )}
-        </div>
-      </div>
+      <Carter product={product} />
     </div>
   );
 };
