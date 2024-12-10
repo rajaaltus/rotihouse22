e07c9edc4ebcdbfa427cc923456fd17cf4a7f443
iROW9 <irow9.ks@gmail.com>
2024-12-10 21:28:08 +0700

    config updated

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 //  import type { Config } from 'drizzle-kit';
 // import * as dotenv from 'dotenv';
 // dotenv.config({ path: '.env.local' });
+import { config } from 'dotenv';
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
@@ -8,10 +9,6 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'mysql',
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
-    port: Number(process.env.DB_PORT),
+    url: process.env.NEXT_PUBLIC_DATABASE_URL!,
   },
 }) 
