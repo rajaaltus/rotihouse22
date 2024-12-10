eb0de633f38f47b5d77ddbd3a19334308f9c8c76
iROW9 <irow9.ks@gmail.com>
2024-12-10 21:56:52 +0700

    updated

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: ["api2.rotihouselao.com", "localhost", "nextui.org"],
   },
-  env: {
-    API_URL: "https://api2.rotihouselao.com",
-    GTM_ID: "G-G0S874M3ST",
-  },
 };
 
 module.exports = nextConfig;
