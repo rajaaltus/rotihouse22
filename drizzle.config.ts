4fc582da02da77498663a36e5d3a0e5c7e5c78dd
iROW9 <irow9.ks@gmail.com>
2024-12-10 21:46:53 +0700

    updated

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,8 @@
 import { config } from 'dotenv';
 import { defineConfig } from "drizzle-kit";
 
+config({ path: '.env' });
+
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './drizzle',
